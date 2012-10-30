1538a8a8c1dc07e03d61a5e73665a12f42c5ff00
Alex Little <alex@alexlittle.net>
2012-10-30 16:08:22 +0100

    adding api

--- api.py
+++ api.py
@@ -1,29 +1,22 @@
-#mquiz/api.py
+# mquiz/api.py
 from django.contrib.auth.models import User
 from tastypie import fields
 from tastypie.resources import ModelResource
-from mquiz.models import Quiz,Question,Response
+from tastypie.authentication import BasicAuthentication
+from mquiz.models import Quiz
 
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
-        resource_name = 'owner'
-
+        resource_name = 'user'
+        fields = ['first_name', 'last_name', 'last_login']
+        allowed_methods = ['get']
+        authentication = BasicAuthentication()
+        
+        
 class QuizResource(ModelResource):
-    owner = fields.ForeignKey(UserResource, 'owner')    
     class Meta:
         queryset = Quiz.objects.all()
+        fields = ['title', 'id']
         resource_name = 'quiz'
-        
-class QuestionResource(ModelResource):
-    owner = fields.ForeignKey(UserResource, 'owner')    
-    class Meta:
-        queryset = Question.objects.all()
-        resource_name = 'question'
-        
-class ResponseResource(ModelResource):
-    owner = fields.ForeignKey(UserResource, 'owner')    
-    class Meta:
-        queryset = Response.objects.all()
-        resource_name = 'response'
