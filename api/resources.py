8011e9801c007c130292858385b5189c5261db07
Alex Little <alex@alexlittle.net>
2012-10-31 10:19:51 +0100

    add custom serializer for quiz json output

--- api/resources.py
+++ api/resources.py
@@ -5,7 +5,7 @@ from tastypie.resources import ModelResource
 from tastypie.authentication import BasicAuthentication
 from mquiz.models import Quiz, Question, QuizQuestion, Response
 from mquiz.api.auth import MquizAPIAuthorization
-from mquiz.api.serializers import PrettyJSONSerializer
+from mquiz.api.serializers import PrettyJSONSerializer, QuizJSONSerializer
 
 
 class UserResource(ModelResource):
@@ -20,25 +20,27 @@ class UserResource(ModelResource):
                
         
 class QuizResource(ModelResource):
+    q = fields.ToManyField('mquiz.api.resources.QuizQuestionResource', 'quizquestion_set', related_name='quiz', full=True)
     class Meta:
         queryset = Quiz.objects.all()
         allowed_methods = ['get']
         fields = ['title', 'id']
         resource_name = 'quiz'
-        serializer = PrettyJSONSerializer()
+        include_resource_uri = False
+        serializer = QuizJSONSerializer()
         
 class QuizQuestionResource(ModelResource):
-    quiz = fields.ToOneField('mquiz.api.resources.QuizResource', 'quiz', full=True)
+    #quiz = fields.ToOneField('mquiz.api.resources.QuizResource', 'quiz', full=True)
     question = fields.ToOneField('mquiz.api.resources.QuestionResource', 'question', full=True)
     class Meta:
         queryset = QuizQuestion.objects.all()
         allowed_methods = ['get']
-        resource_name = 'quiz/question'
         include_resource_uri = False
-        serializer = PrettyJSONSerializer()
+        serializer = QuizJSONSerializer()
         
 class QuestionResource(ModelResource):
-    r = fields.ToManyField('mquiz.api.resources.ResponseResource', 'response_set', related_name='question', full=True)   
+    #quiz = fields.ToManyField('mquiz.api.resources.QuizQuestionResource', 'quiz', full=True)
+    #r = fields.ToManyField('mquiz.api.resources.ResponseResource', 'response_set', related_name='question', full=True)   
     class Meta:
         queryset = Question.objects.all()
         allowed_methods = ['get']
--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from django.core.serializers import json
 from django.utils import simplejson
 from tastypie.serializers import Serializer
+import time
 
 class PrettyJSONSerializer(Serializer):
     json_indent = 2
@@ -8,5 +9,21 @@ class PrettyJSONSerializer(Serializer):
     def to_json(self, data, options=None):
         options = options or {}
         data = self.to_simple(data, options)
+        return simplejson.dumps(data, cls=json.DjangoJSONEncoder,
+                sort_keys=True, ensure_ascii=False, indent=self.json_indent)
+        
+        
+class QuizJSONSerializer(Serializer):
+    json_indent = 2
+
+    def to_json(self, data, options=None):
+        options = options or {}
+        data = self.to_simple(data, options)
+        for question in data['q']:
+            del question['id']
+            del question['order']
+            for qkey, qvalue in question['question'].items():
+                question[qkey] = qvalue
+            del question['question']
         return simplejson.dumps(data, cls=json.DjangoJSONEncoder,
                 sort_keys=True, ensure_ascii=False, indent=self.json_indent)
