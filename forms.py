95baadb93b5db10806b6dbf4be743cfbbeda881d
Alex Little <alex@alexlittle.net>
2012-11-22 18:13:08 +0100

    create quiz (but not questions etc)

--- forms.py
+++ forms.py
@@ -1,11 +1,27 @@
 #mquiz/forms.py
 from django import forms
-from django.core.validators import validate_email
-from django.contrib.auth.models import User
+from django.forms import ModelForm
 from django.utils.translation import ugettext as _
+from mquiz.models import Quiz
 
-class QuizForm(forms.Form):
+class QuizForm(ModelForm):
+    class Meta:
+        model = Quiz
+        fields = ('title', 'description')
+        widgets = {
+            'title': forms.TextInput(),     
+            'description': forms.Textarea(attrs={'cols': 80, 'rows': 3}),
+        }
+    
+    
+class QuestionForm(forms.Form):
+    title = forms.CharField(error_messages={'invalid': _(u'Please enter a title.')},
+                            required=True,
+                            widget=forms.Textarea(attrs={'cols': 80, 'rows': 3}),)
+    
+    
+class ResponseForm(forms.Form):
     title = forms.CharField(max_length=200,
-        error_messages={'invalid': _(u'Please enter a title.')},
-        required=True)
+                            error_messages={'invalid': _(u'Please enter a title.')},
+                            required=True,)
 
--- views.py
+++ views.py
@@ -1,23 +1,36 @@
-#mquiz/views.py
+# mquiz/views.py
 from django.shortcuts import render,render_to_response
 from django.template import RequestContext
 from django.contrib.auth import (authenticate, logout, views)
 from django.contrib.auth.models import User
+from django.forms.formsets import formset_factory
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 from mquiz.models import Quiz
-from forms import QuizForm
+from forms import QuizForm, QuestionForm
 
 def home_view(request):
     latest_quiz_list = Quiz.objects.filter(draft=0).order_by('-created_date')[:10]
     return render_to_response('mquiz/home.html',{'latest_quiz_list': latest_quiz_list}, context_instance=RequestContext(request))
 
-def create(request):
+def create_quiz(request):
+    #QuestionFormSet = formset_factory(QuestionForm,extra=3,)
     if request.method == 'POST':
-        form = QuizForm(request.POST)
+        quiz_form = QuizForm(request.POST)
+        #question_formset = QuestionFormSet(request.POST,prefix='questions')
+        if quiz_form.is_valid():
+            quiz = quiz_form.save(commit=False)
+            quiz.owner_id = request.user.id
+            quiz.save()
+            messages.info(request, "Your quiz was saved")
+            return HttpResponseRedirect('saved/')
     else:
-        form = QuizForm() # An unbound form
+        quiz_form = QuizForm() # An unbound form
+        #question_formset = QuestionFormSet(prefix='questions',)
+        
 
-    return render(request, 'mquiz/quiz/quiz.html', {'form': form,})
+    return render(request, 'mquiz/quiz/quiz.html', {'quiz_form': quiz_form,})
 
 
 
